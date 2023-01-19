47616d482f038f5a66c3a045de7713442e70e503
Yash-kaushik29 <115134639+Yash-kaushik29@users.noreply.github.com>
2023-01-19 09:49:59 +0530

    Create First repeating element.cpp

---
+++ Yash-kaushik29/First repeating element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    // Function to return the position of the first repeating element.
+    int firstRepeated(int arr[], int n) {
+        // code here
+        unordered_map <int,int> hmap ;
+        for (int i = 0 ; i< n ; i++)
+        {
+            hmap[arr[i]]++;
+        }
+        for (int i= 0 ; i<n ;i++)
+        {
+            if(hmap[arr[i]]>1)
+            return i+1;
+        }
+        return -1;
+    }
+};
