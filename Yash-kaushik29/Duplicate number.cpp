09ca9058620abc9925598a976e8f003ff84fc7c1
Yash-kaushik29 <115134639+Yash-kaushik29@users.noreply.github.com>
2023-01-19 10:20:16 +0530

    Create Duplicate number.cpp

---
+++ Yash-kaushik29/Duplicate number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        
+         unordered_map<int,int> m;
+         vector<int> v;
+         int a;
+        
+       for(int i=0 ; i<nums.size() ; i++)
+       {
+           m[nums[i]]++;
+       }
+       
+       for(auto it:m)
+       {
+           if(it.second > 1)
+           a= it.first;
+       }
+       
+       return a;
+        
+    }
+};
