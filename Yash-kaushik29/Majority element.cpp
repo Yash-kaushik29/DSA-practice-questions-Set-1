ccce53e0213d0162710ca40f02ebc42206821d0d
Yash-kaushik29 <115134639+Yash-kaushik29@users.noreply.github.com>
2023-01-19 10:03:12 +0530

    Create Majority element.cpp

---
+++ Yash-kaushik29/Majority element.cpp
@@ -0,0 +1,24 @@
+class Solution{
+  public:
+     // Function to find majority element in the array
+    // a: input array
+    // size: size of input array
+    int majorityElement(int a[], int size)
+    {
+        unordered_map<int,int> m;
+        
+       for(int i=0 ; i<size ; i++)
+       {
+           m[a[i]]++;
+       }
+       
+       for(auto it:m)
+       {
+           if(it.second > size/2)
+           return it.first;
+       }
+       
+       return -1;
+        
+    }
+};
