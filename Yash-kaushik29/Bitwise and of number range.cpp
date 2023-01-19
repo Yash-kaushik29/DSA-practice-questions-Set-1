6236b8dc581345c21c1c410fcf92f30e3d8323ce
Yash-kaushik29 <115134639+Yash-kaushik29@users.noreply.github.com>
2023-01-19 10:11:38 +0530

    Create Bitwise and of number range.cpp

---
+++ Yash-kaushik29/Bitwise and of number range.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int rangeBitwiseAnd(int left, int right) {
+     
+        int c = 0;
+        
+        while( left != right)
+        {
+            left = left >> 1;
+            right = right >> 1;
+            c++;
+        }
+        
+        return left<<c;
+        
+    }
+};
