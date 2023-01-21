11f10396495495641f750998a53518392d8eb24c
Yash-kaushik29 <115134639+Yash-kaushik29@users.noreply.github.com>
2023-01-21 10:24:40 +0530

    Create Spiral Matrix.cpp

---
+++ Yash-kaushik29/Spiral Matrix.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        
+        vector<int> ans;
+        int row=matrix.size();
+        int col=matrix[0].size();
+        
+        int count=0;
+        int total=col*row;
+
+        int startingRow=0;
+        int startingCol=0;
+        int endingRow=row-1;
+        int endingCol=col-1;
+
+        while(count<total) {
+            for(int i=startingCol ; count<total && i<=endingCol ; i++) {
+            ans.push_back(matrix[startingRow][i]);
+            count++;
+        }
+           startingRow++;
+
+         for(int i=startingRow ; count<total && i<=endingRow ; i++) {
+            ans.push_back(matrix[i][endingCol]);
+            count++;
+        }
+           endingCol--;
+
+         for(int i=endingCol ; count<total && i>=startingCol ; i--) {
+            ans.push_back(matrix[endingRow][i]);
+            count++;
+        }
+            endingRow--;
+
+         for(int i=endingRow ; count<total && i>=startingRow ; i--) {
+            ans.push_back(matrix[i][startingCol]);
+            count++;
+        }
+            startingCol++;
+     }
+
+        return ans;
+    }
+};
