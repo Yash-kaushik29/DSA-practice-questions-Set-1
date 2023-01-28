25d0c74bc79767f049c341a65b5c5a43bc3d9110
Yash-kaushik29 <115134639+Yash-kaushik29@users.noreply.github.com>
2023-01-28 16:19:50 +0530

    Update Reverse string.cpp

--- Yash-kaushik29/Reverse string.cpp
+++ Yash-kaushik29/Reverse string.cpp
@@ -1,3 +1,5 @@
+// Normal approach
+
 class Solution {
 public:
     void reverseString(vector<char>& s) {
@@ -8,5 +10,35 @@ public:
         while(st<e) {
             swap(s[st++],s[e--]);
         }
+    }
+};
+
+// Using recursion
+
+class Solution {
+public:
+    void reverse(vector<char>& ans, int i , int j) {
+
+        // base case
+        if(i>j)
+        return;
+
+        swap(ans[i],ans[j]);
+        //recursive call
+        i++;
+        j--;
+
+        return reverse(ans,i,j);
+
+    }
+    
+    vector<char> reverseString(vector<char>& v) {
+        
+        int s=0;
+        int e=v.size()-1;
+
+        reverse(v,s,e);
+        return v;
+  
     }
 };
