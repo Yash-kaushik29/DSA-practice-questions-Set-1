b7238043a331b7dfe40eae7c713351cef782b4c5
Yash-kaushik29 <115134639+Yash-kaushik29@users.noreply.github.com>
2023-01-29 10:30:01 +0530

    Create Minimum operations to make a and b equal.cpp

---
+++ Yash-kaushik29/Minimum operations to make a and b equal.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int solve(int a, int b) {
+        // code here
+        if(a==b) return 0;
+        
+        if((a&b)==a || (a&b)==b)
+        return 1;
+        
+        return 2;
+    }
+};
