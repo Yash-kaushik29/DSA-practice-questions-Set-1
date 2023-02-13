caca824594c26893a7810ba8577f4b1bc878b9b9
Yash-kaushik29 <115134639+Yash-kaushik29@users.noreply.github.com>
2023-02-13 19:45:39 +0530

    Create Next Permutation.cpp

---
+++ Yash-kaushik29/Next Permutation.cpp
@@ -0,0 +1,23 @@
+int index = -1;
+        int n = nums.size();
+
+        for(int i=n-1; i>0; i--) {
+            if(nums[i] > nums[i-1]) {
+                index = i;
+                break;
+            }
+        }
+
+        if(index == -1)
+          reverse(nums.begin(),nums.end());
+
+        else  {
+            int prev = index;
+            for(int i=index+1; i<n; i++) {
+                if(nums[i]>nums[index-1] && nums[i]<=nums[prev]) {
+                    prev = i;
+                }
+            }
+            swap(nums[index-1],nums[prev]);
+            reverse(nums.begin()+index,nums.end());
+        }
