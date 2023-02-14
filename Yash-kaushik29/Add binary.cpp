a434122428214a670972d086107b10c2417d2e28
Yash-kaushik29 <115134639+Yash-kaushik29@users.noreply.github.com>
2023-02-14 11:54:54 +0530

    Create Add binary.cpp

---
+++ Yash-kaushik29/Add binary.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+       int carry=0;
+       string ans;
+
+       int i = a.length()-1;
+       int j = b.length()-1;
+
+       while(i>=0 || j>=0) {
+           int a_bit = i>=0 ? a[i--] - '0' : 0;
+           int b_bit = j>=0 ? b[j--] - '0' : 0;
+
+           int sum = a_bit + b_bit + carry;
+           ans.insert(ans.begin(),(sum % 2) +'0');
+
+           carry = sum/2;
+       }  
+       if(carry) {
+           ans.insert(ans.begin(), '1');
+       }
+
+       return ans; 
+    }
+};
