f2e9ace738c5bb5101e67afb71f22208d58e0311
Yash-kaushik29 <115134639+Yash-kaushik29@users.noreply.github.com>
2023-02-15 20:56:58 +0530

    Create Minimize the Heights II.cpp

---
+++ Yash-kaushik29/Minimize the Heights II.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    int getMinDiff(int arr[], int n, int k) {
+        // code here
+        if(n == 0)
+          return 0;
+          
+        sort(arr,arr+n);
+        int diff = arr[n-1]-arr[0];
+        int maxi,mini;
+        
+        for(int i=1; i<n; i++) {
+            
+            if(arr[i]-k < 0)  continue;
+            
+            maxi = max(arr[i-1]+k,arr[n-1]-k);
+            mini = min(arr[0]+k,arr[i]-k);
+            
+            diff = min(diff,maxi-mini);
+        }
+        return diff;
+    }
+};
