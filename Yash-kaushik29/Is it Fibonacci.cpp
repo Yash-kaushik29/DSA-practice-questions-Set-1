d11c9f2bc958694b8a4bec257cf23897035a7cc7
Yash-kaushik29 <115134639+Yash-kaushik29@users.noreply.github.com>
2023-02-17 10:45:38 +0530

    Create Is it Fibonacci.cpp

---
+++ Yash-kaushik29/Is it Fibonacci.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    long long solve(int N, int K, vector<long long> GeekNum) {
+        // code here
+        long long preSum = 0;
+        queue<long long> q;
+        for (auto num : GeekNum) {
+            q.push(num);
+            preSum += num;
+        }
+        if (N <= K) return GeekNum[N - 1];
+        long long curr = preSum;
+        for (int i = K; i < N; ++i){
+            curr = preSum;
+            q.push(curr);
+            preSum += curr;
+            preSum -= q.front();
+            q.pop();
+        }
+        return curr;
+    }
+};
