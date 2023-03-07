a9d84e02ad4ee8900bb2f5da64762d1cf0975a85
Yash-kaushik29 <115134639+Yash-kaushik29@users.noreply.github.com>
2023-03-07 13:21:59 +0530

    Create Minimum Time to Complete Trips.cpp

---
+++ Yash-kaushik29/Minimum Time to Complete Trips.cpp
@@ -0,0 +1,33 @@
+typedef long long ll;
+
+class Solution {
+public:
+    bool check(vector<int> &time, ll mid, ll t){
+        ll trips = 0;
+        for(auto &i:time){
+            trips += (mid/i);
+            if(trips >= t) return true;
+        }
+        return false;
+    }
+
+    long long minimumTime(vector<int>& time, int totalTrips) {
+        int n = time.size();
+        sort(time.begin(), time.end());
+
+        ll l = (ll)time[0];
+        ll r = (ll)time[n-1]*(ll)totalTrips;
+
+        while(l<r) {
+            ll mid = l + (r-l)/2;
+
+            if(check(time, mid, (ll)totalTrips)){
+                r = mid;
+            }
+            else{
+                l = mid+1;
+            }
+        }
+        return l;
+    }
+};
