af5b0fa1ef1b6872867691233a5b0147174dffb6
Yash-kaushik29 <115134639+Yash-kaushik29@users.noreply.github.com>
2023-03-13 10:23:47 +0530

    Update Rat in a maze.cpp

--- Yash-kaushik29/Rat in a maze.cpp
+++ Yash-kaushik29/Rat in a maze.cpp
@@ -89,3 +89,60 @@ class Solution{
         
     }
 };
+
+
+// Approach 2: Backtrack
+class Solution{
+    public:
+    bool isSafe(int x, int y, vector<vector<int>> &m, int n, vector<vector<bool>> &visited) {
+        
+        if((x>=0 && x<n) && (y>=0 && y<n) && (visited[x][y] == 0) && m[x][y] ==1) {
+            return true;
+        }
+        else
+            return false;
+    }
+    
+    void solve(int x, int y, vector<vector<int> > &m, int n, vector<string> &ans, vector<vector<bool>> &visited, string path) {
+        
+        // base case
+        if((x == n-1) && (y == n-1)) {
+            ans.push_back(path);
+            return ;
+        }
+        
+        visited[x][y] = 1;
+        // Down movement
+        if(isSafe(x+1, y, m, n, visited)) {
+            solve(x+1, y, m, n, ans, visited, path + 'D');
+        }
+        
+        // Left movement
+        if(isSafe(x, y-1, m, n, visited)) {
+            solve(x, y-1, m, n, ans, visited, path + 'L');
+        }
+        
+        // Right movement
+        if(isSafe(x, y+1, m, n, visited)) {
+            solve(x, y+1, m, n, ans, visited, path + 'R');
+        }
+        
+        // Up movement
+        if(isSafe(x-1, y, m, n, visited)) {
+            solve(x-1, y, m, n, ans, visited, path + 'U');
+        }
+        visited[x][y] = 0;
+    }
+    
+    vector<string> findPath(vector<vector<int>> &m, int n) {
+        // Your code goes here
+        vector<string> ans;
+        vector<vector<bool> > visited(n, vector<bool>(n,0));
+        string path = "";
+        
+        if(m[0][0] == 0)
+           return ans;
+        
+        solve(0, 0, m, n, ans, visited, path);
+    }
+};
