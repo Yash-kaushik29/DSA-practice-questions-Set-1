48304a570bf3e7df55be3e31c72ff826fc888613
Yash-kaushik29 <115134639+Yash-kaushik29@users.noreply.github.com>
2023-03-19 11:44:26 +0530

    Update Capacity To Ship Packages Within D Days.cpp

--- Yash-kaushik29/Capacity To Ship Packages Within D Days.cpp
+++ Yash-kaushik29/Capacity To Ship Packages Within D Days.cpp
@@ -1,36 +1,44 @@
 class Solution {
 public:
-    bool check(vector<int>& weights, int days, int capacity){
+    bool isPossible(vector<int>& weights, int days, int mid) {
+        int dayCount=1;
+        int weight=0;
 
-        int requiredDays = 1;
-        int currWeight = 0;
-        for(int i : weights){
-            if(currWeight + i > capacity){
-                requiredDays++;
-                currWeight = 0;
+        for(int i=0; i<weights.size(); i++) {
+            if(weights[i]+weight <= mid) {
+                weight+=weights[i];
             }
-            currWeight += i;
+            else {
+                dayCount++;
+                weight=weights[i];
+                if(dayCount>days || weights[i]>mid) {
+                    return false;
+                }
+            }
         }
-        if(requiredDays > days) return false;
         return true;
-    }
+    } 
     
     int shipWithinDays(vector<int>& weights, int days) {
-        int left = 0;
-        int right = 0;
-        for(int i : weights){
-            left = max(left, i);
-            right += i;
+        int s=0;
+        int sum=0;
+        int ans=-1;
+
+        for(int i=0; i<weights.size(); i++) {
+            sum+=weights[i];
         }
-        int mid;
-        int ans = right;
-        while(left <= right){
-            mid = (left + right) / 2;
-            if(check(weights, days, mid)){
-                ans = mid;
-                right = mid - 1;
-            } else {
-                left = mid + 1;
+
+        int e=sum;
+
+        while(s<=e) {
+            int mid=s+(e-s)/2;
+
+            if(isPossible(weights, days, mid)) {
+                ans=mid;
+                e=mid-1;
+            }
+            else {
+                s=mid+1;
             }
         }
         return ans;
