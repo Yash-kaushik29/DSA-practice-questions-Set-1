a105caf266e781712b62f9c4ed7a36a79add1aa4
Yash-kaushik29 <115134639+Yash-kaushik29@users.noreply.github.com>
2023-03-21 20:42:16 +0530

    Update Convert array to reduced form.cpp

--- Yash-kaushik29/Convert array to reduced form.cpp
+++ Yash-kaushik29/Convert array to reduced form.cpp
@@ -3,17 +3,17 @@ public:
 	// Converts arr[0..n-1] to reduced form.
 	void convert(int arr[], int n) {
 	    // code here
-	   vector<int> v(arr,arr+n);
-	   sort(v.begin(),v.end());
-	   unordered_map<int,int> m;
-	   
-	   for(int i=0 ; i<n ; i++)  {
-	       m[v[i]]=i;
-	   }
-	   
-	   for(int i=0 ; i<n ; i++)  {
-	       arr[i]=m[arr[i]];
-	   }
+	    vector<pair<int, int> > v;
+	    
+	    for(int i=0; i<n; i++) {
+	        v.push_back({arr[i],i});
+	    }
+	    
+	    sort(v.begin(),v.end());
+	    
+	    for(int i=0; i<n; i++) {
+	        arr[v[i].second]=i;
+	    }
 	   
 	}
 
