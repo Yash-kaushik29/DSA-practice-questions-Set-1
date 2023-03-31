c3b563a3574877bf1a39fd955b96e1e4787b7184
Yash-kaushik29 <115134639+Yash-kaushik29@users.noreply.github.com>
2023-03-31 10:35:53 +0530

    Update Find position of set bit.cpp

--- Yash-kaushik29/Find position of set bit.cpp
+++ Yash-kaushik29/Find position of set bit.cpp
@@ -2,17 +2,13 @@ class Solution {
   public:
     int findPosition(int N) {
         // code here
+        if(N & N-1 || N==0) return -1;
+        
         int pos=0;
         
-        if(N==0)
-        return -1;
-        
-        if(N & (N-1))
-        return -1;
-        
-        while(N>0) {
+        while(N) {
             pos++;
-            N = N>>1;
+            N=N>>1;
         }
         
         return pos;
