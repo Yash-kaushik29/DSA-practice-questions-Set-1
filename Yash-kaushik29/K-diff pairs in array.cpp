08bd00c595801d5f0ad80b76aec8ef046e29f69c
Yash-kaushik29 <115134639+Yash-kaushik29@users.noreply.github.com>
2023-04-03 18:38:00 +0530

    Update K-diff pairs in array.cpp

--- Yash-kaushik29/K-diff pairs in array.cpp
+++ Yash-kaushik29/K-diff pairs in array.cpp
@@ -1,23 +1,28 @@
 // Method 1: using heap
 
-int n = nums.size();
-        unordered_map<int, int> map;
-        for(auto i : nums){
-            map[i]++;
-        }
-        int ans = 0;
-        for(auto x : map){
-            // if k == 0 instead of checking x.first+k check its frequency
-            if(k == 0){
-                if(x.second > 1){
-                    ans++;
-                }
-            }else if(map.find(x.first+k) != map.end()){
-                    ans++;
-            }
-        }
-        return ans;
- }
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+       unordered_map<int, int> m;
+       int ans=0;
+
+       for(auto i: nums) {
+           m[i]++;
+       }
+
+       for(auto i: m) {
+           if(k==0) {
+               if(i.second > 1) {
+                   ans++;
+               }
+           }
+           else if(m.find(i.first+k) != m.end()) {
+                 ans++;
+           }
+       }
+       return ans;
+    }   
+};
 
 // Method 2: using two pointers
 
