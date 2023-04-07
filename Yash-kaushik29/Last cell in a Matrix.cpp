24be99b431e055bac21a422236012b536f3b1f87
Yash-kaushik29 <115134639+Yash-kaushik29@users.noreply.github.com>
2023-04-07 18:33:05 +0530

    Update Last cell in a Matrix.cpp

--- Yash-kaushik29/Last cell in a Matrix.cpp
+++ Yash-kaushik29/Last cell in a Matrix.cpp
@@ -1,20 +1,27 @@
 class Solution{
     public:
     pair<int,int> endPoints(vector<vector<int>> matrix, int R, int C){
-        //code here
-        vector<pair<int, int>> dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        int n = matrix.size(), m = matrix[0].size();
+        
         int i=0, j=0, k=0;
+        vector<pair<int, int> > dir = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+        
         pair<int, int> ans;
-        while(i>=0 && j>=0 && i< n && j<m){
-            ans = {i, j};
-            if(matrix[i][j]==1){
-                matrix[i][j]=0;
-                k = (k+1)%4; 
+        
+        while(i>=0 and j>=0 and i<R and j<C) {
+            
+            // store last cell index
+            ans = {i,j};
+            
+            if(matrix[i][j] == 1) {
+                matrix[i][j] = 0;
+                // change direction
+                k = (k+1) % 4;
             }
-            i = i+dir[k].first;
-            j = j+dir[k].second;
+            
+            i += dir[k].first;
+            j += dir[k].second;
         }
+        
         return ans;
     }
 };
