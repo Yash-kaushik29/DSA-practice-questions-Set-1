6431a942d272d210c8a5bfe1b8d8f9f1914c095a
Yash-kaushik29 <115134639+Yash-kaushik29@users.noreply.github.com>
2023-04-11 13:35:23 +0530

    Update Max min Height.cpp

--- Yash-kaushik29/Max min Height.cpp
+++ Yash-kaushik29/Max min Height.cpp
@@ -1,53 +1,43 @@
 class Solution
 {
     public:
-        bool isCheck(vector<int> &arr, int days, int cs, long long int h){
+        bool isPossible(int n, vector<int> a, int k, int w, long long int height) {
             
-            int n=arr.size();
-            vector<long long> waterSupply(n,0);
-            if(arr[0]<h){
-                waterSupply[0]=h-arr[0];
-                days-=(h-arr[0]);
-            }
-            if(days<0){
-                return false;
-            }
-            for(int i=1;i<n;i++) {
-                waterSupply[i]=waterSupply[i-1];
-                int actualHeight=arr[i];
-                if(i>=cs){
-                    actualHeight+=(waterSupply[i]-waterSupply[i-cs]);
-                }
-                else{
-                    actualHeight+=waterSupply[i];
-                }
-                if(actualHeight<h){
-                    waterSupply[i]+=(h-actualHeight);
-                    days-=(h-actualHeight);
-                }
-                if(days<0){
-                    return false;
+            for(int i=0; i<n; i++) {
+                if(a[i] < height) {
+                    int days = height - a[i];
+                    if(days > k) return false;
+                    k-=days;
+                    a[i] = height;
+                    
+                    int j=i+1;
+                    while(j<n and j<i+w) {
+                        a[j]+=days;
+                        j++;
+                    }
                 }
             }
             return true;
         }
       
-        long long int maximizeMinHeight(int n,int k,int w,vector <int> &a)
-        {
-            // code here
-            long long int ans=-1;
-            long long int mini=*min_element(a.begin(),a.end());
-            long long int maxi=INT_MAX/2;
-            while(mini<=maxi){
-                int guessHeight=(maxi+mini)/2;
-                if(isCheck(a,k,w,guessHeight)==true){
-                    ans=guessHeight;
-                    mini=guessHeight+1;
+      
+        long long int maximizeMinHeight(int n,int k,int w,vector <int> &a) {
+            
+            long long int mini = *min_element(a.begin(), a.end());
+            long long int maxi = mini+k;
+            long long int ans;
+            
+            while(mini <= maxi) {
+                long long int mid = (mini+maxi)/2;
+                
+                if(isPossible(n, a, k, w, mid)) {
+                    ans = mid;
+                    mini = mid+1;
                 }
-                else{
-                    maxi=guessHeight-1;
+                else {
+                    maxi = mid-1;
                 }
             }
-            return ans;  
+            return ans;
         }
 };
