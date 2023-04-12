171e870102fef728df0acd61be9335aab9ae3068
Arin Yadav <98683112+arinyadav@users.noreply.github.com>
2023-04-12 18:04:03 +0530

    Update 3Sum.cpp

--- Yash-kaushik29/3Sum.cpp
+++ Yash-kaushik29/3Sum.cpp
@@ -4,7 +4,6 @@ public:
         int target = 0;
         sort(nums.begin(), nums.end());
         set<vector<int>> s;
-        vector<vector<int>> output;
         for (int i = 0; i < nums.size(); i++){
             int j = i + 1;
             int k = nums.size() - 1;
@@ -21,9 +20,8 @@ public:
                 }
             }
         }
-        for(auto triplets : s)
-            output.push_back(triplets);
-        return output;   
+       vector<vector<int>> ans(st.begin(),st.end());
+        return ans;  
     }
 };
 
