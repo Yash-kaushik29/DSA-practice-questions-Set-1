531ccdc68c7e306eae5f90cc09652c0142c44b9a
Arin Yadav <98683112+arinyadav@users.noreply.github.com>
2023-04-12 14:12:36 +0530

    Update 3Sum.cpp

--- Yash-kaushik29/3Sum.cpp
+++ Yash-kaushik29/3Sum.cpp
@@ -26,3 +26,52 @@ public:
         return output;   
     }
 };
+
+
+
+
+
+//youTube video link " https://www.youtube.com/watch?v=QLec4VB4Ol0 "
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>>result;
+        int sum,n=nums.size();
+        sort(nums.begin(),nums.end());
+
+        for(int i=0;i<n-2;i++)
+        {
+          if(i==0 || nums[i]!=nums[i-1]) {
+              int left = i+1;
+              int right = n-1;
+              while(left<right) {
+                 sum = nums[i]+nums[left]+nums[right]; 
+                 if(sum==0) {
+                 result.push_back({nums[i],nums[left],nums[right]});
+                
+                //imp
+                 while(left<right && nums[left]==nums[left+1])
+                 left++;
+                 
+                 //imp
+                 while(left<right && nums[right]==nums[right-1])
+                 right--;
+                 
+                 left++;
+                 right--;
+                 }
+                 
+                  //imp
+                 else if (sum<0) 
+                 left++;
+                 
+                  //imp
+                 else
+                 right--;
+              }
+          }
+        }
+        return result;
+    }
+};
