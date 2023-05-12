b7b29d918b4ace4b76730dfc39c4e4fba8671bc2
Yash-kaushik29 <115134639+Yash-kaushik29@users.noreply.github.com>
2023-05-12 11:45:55 +0530

    Update Median of two sorted arrays.cpp

--- Yash-kaushik29/Median of two sorted arrays.cpp
+++ Yash-kaushik29/Median of two sorted arrays.cpp
@@ -26,31 +26,37 @@ public:
 // Optimized solution
 
 double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if(nums1.size()>nums2.size())
-            return findMedianSortedArrays(nums2,nums1);
-        int s = 0, e = nums1.size();
-        while(s<=e){
-            int i1 = (s+e)>>1;
-            int i2 = ((nums1.size()+nums2.size()+1)>>1)-i1;
+        if(nums1.size() > nums2.size()) 
+          return findMedianSortedArrays(nums2, nums1);
 
-            int max1 = (i1==0?INT_MIN:nums1[i1-1]);
-            int max2 = (i2==0?INT_MIN:nums2[i2-1]);
-            int min1 = (i1==nums1.size()?INT_MAX:nums1[i1]);
-            int min2 = (i2==nums2.size()?INT_MAX:nums2[i2]);
+        int n1=nums1.size();
+        int n2=nums2.size();
+        int low=0, high=n1;
 
-            if(max1<=min2 && max2<=min1){
-                if((nums1.size()+nums2.size())&1){
-                    return (double)max(max1,max2);
-                }else{
-                    return ((double(max(max1,max2)+min(min1,min2))))/2;
+        while(low <= high) {
+            int cut1 = (low+high)/2;
+            int cut2 = (n1+n2+1) / 2 - cut1;
+
+            int left1 = cut1 == 0 ? INT_MIN : nums1[cut1-1];
+            int left2 = cut2 == 0 ? INT_MIN : nums2[cut2-1];
+
+            int right1 = cut1 == n1 ? INT_MAX : nums1[cut1];
+            int right2 = cut2 == n2 ? INT_MAX : nums2[cut2];
+
+            if(left1 <= right2 && left2 <= right1) {
+                if((n1+n2) % 2 == 0) {
+                    return (max(left1, left2) + min(right1, right2))/2.0;
+                }
+                else {
+                    return max(left1, left2);
                 }
             }
-            else if(max1>min2){
-                e = i1-1;
+            else if(left1 > right2) {
+                high = cut1-1;
             }
-            else{
-                s = i1+1;
+            else {
+                low = cut1+1;
             }
         }
-        return -1.0;
+        return 0.0; 
     }
