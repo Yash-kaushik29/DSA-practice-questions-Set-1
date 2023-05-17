4adfcf24ed0c8981635a2ffe7ae6a58ad1315f52
Yash-kaushik29 <115134639+Yash-kaushik29@users.noreply.github.com>
2023-05-17 23:38:36 +0530

    Update Minimum Limit of Balls in a Bag.cpp

--- Yash-kaushik29/Minimum Limit of Balls in a Bag.cpp
+++ Yash-kaushik29/Minimum Limit of Balls in a Bag.cpp
@@ -1,67 +1,37 @@
-// Binary search approach
-
 class Solution {
 public:
-    int minimumSize(vector<int>& nums, int maxOperations) {
-        int e= *max_element(nums.begin(),nums.end());
-        int s=1;
-        int ans;
+    bool isPossible(vector<int> nums, int o, int penalty) {
+        int ops=0;
 
-        if(maxOperations==1 && nums[0]>1)
-        return nums[0]/2;
-
-        if(maxOperations==1 && nums[0]==1)
-        return nums[0]; 
-
-        while(s<=e) {
-            int penalty=s+(e-s)/2;
-            int operations=0;
-
-            for(int i=0; i<nums.size(); i++) {
-                if(nums[i]>penalty) {
-                    if(nums[i]%penalty==0) {
-                    operations+=(nums[i]/penalty)-1;
-                    }
-                    else {
-                    operations+=nums[i]/penalty;
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] > penalty) {
+                if(nums[i] % penalty == 0) {
+                    ops += nums[i] / penalty - 1;
+                } else {
+                    ops += nums[i] / penalty;
                 }
-              }
-            }
-            if(operations <= maxOperations) {
-                ans=penalty;
-                e=penalty-1;
-            }
-
-            else {
-                s=penalty+1;
+                if(ops > o) return false;
             }
         }
-
-        return ans;
-
+        return true;
     }
-};
 
-
-// Optimised binary search
-
-class Solution {
-public:
-    int minimumSize(vector<int>& nums, int k) {
-        int right = INT_MAX;
-        int cnt = 0,ans = 0, left = 1,mid;
-        while(left < right){
-            mid = ((right-left)>>1)+left;
-            cnt = 0;
-            for(auto &i: nums){
-                cnt += ((i-1)/mid);
+    int minimumSize(vector<int>& nums, int o) {
+        int r = *max_element(nums.begin(), nums.end());
+        int l = 1;
+        int res = r;
+        while(l <= r) {
+            int penalty = (l + r) / 2;
+            
+            if(isPossible(nums, o, penalty)) {
+                res = penalty;
+                r = penalty-1;
             }
-            if(cnt>k){
-                left = mid+1;
-            }else{
-                right = mid;
+            
+            else {
+                l = penalty + 1;
             }
         }
-        return left;
+        return res;
     }
 };
