f458be743c2003f9e2497ebd398e24315e7dfe91
Yash-kaushik29 <115134639+Yash-kaushik29@users.noreply.github.com>
2023-05-27 23:21:05 +0530

    Update Parenthesis Checker.cpp

--- Yash-kaushik29/Parenthesis Checker.cpp
+++ Yash-kaushik29/Parenthesis Checker.cpp
@@ -4,36 +4,27 @@ class Solution
     //Function to check if brackets are balanced or not.
     bool ispar(string x)
     {
-        // Your code here
         stack<char> st;
-    
-        for(int i=0; i<x.length(); i++) {
-        char ch = x[i];
-        if(ch == '(' || ch == '[' || ch == '{') {
-          st.push(ch);
-        }
-        else {
-            if(!st.empty()) {
-                char top = st.top();
-                
-                if((ch == ')' && top == '(') || (ch == '}' && top == '{') || (ch == ']' && top == '[') ) {
-                    st.pop();
+        
+        for(auto ch: x) {
+            if(ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            }
+            else {
+                if(!st.empty()) {
+                    char top = st.top();
+                    if((ch==')' && top=='(') || (ch=='}' && top=='{') || (ch==']' && top=='[')) {
+                        st.pop();
+                    }
+                    else 
+                        return false;
                 }
                 else {
                     return false;
                 }
             }
-            else {
-                return false;
-            }
         }
-       }
-     
-      if(st.empty())
-        return true;
-     
-      else 
-        return false;
+        
+        return st.empty() ? true : false;
     }
-
 };
